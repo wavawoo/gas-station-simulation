397d9cc71f234e35da733aca3cda7d8d744cd0b8
why seven? <kavvxxz@gmail.com>
2026-01-04 22:26:32 +0300

    Create Enums.cs

---
+++ GasStationSim/Enums.cs
@@ -0,0 +1,5 @@
+namespace GasStationSim
+{
+    public enum TankSide { Left, Right }          // Сторона бензобака у машины
+    public enum PumpAccess { LeftOnly, RightOnly, Both } // Доступность колонки
+}
